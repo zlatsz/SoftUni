0ef3163903e47bf832e41fe8f99d41d53048b59c
zlatsz <zlatsz@abv.bg>
2020-08-08 01:10:19 +0300

    Unit test

--- Spring-Advanced/Sister_Art_Store/src/test/java/store/controller/ProductControllerTest.java
+++ Spring-Advanced/Sister_Art_Store/src/test/java/store/controller/ProductControllerTest.java
@@ -1,19 +1,5 @@
 //package store.controller;
 //
-//import com.kyan7.cinephilia.domain.entities.MovieTheater;
-//import com.kyan7.cinephilia.repository.MovieTheaterRepository;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
 //
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
