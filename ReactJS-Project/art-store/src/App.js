d938691928bd79d95074e96be3ae34bb222e1746
zlatsz <zlatsz@abv.bg>
2021-03-19 00:18:47 +0200

    Landing Page Header

--- ReactJS-Project/art-store/src/App.js
+++ ReactJS-Project/art-store/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { Route, Link, NavLink, Redirect, Switch } from 'react-router-dom';
+
+import Header from './components/landing-page/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className=".app">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
